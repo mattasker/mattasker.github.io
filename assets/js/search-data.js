d182869ce23db890406eea4fcef3e717508ec197
mattasker <36448551+mattasker@users.noreply.github.com>
2025-07-26 18:03:03 +0000

    Deploying to gh-pages from @ mattasker/mattasker.github.io@8f4373bb694014e1619a3096df91cc4d596b0b6e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,51 +62,31 @@ ninja.data = [{
             },},{id: "news-a-new-pre-print-collaborating-with-colleagues-at-virginia-tech-is-out-we-investigate-fixation-and-extinction-in-time-fluctuating-spatially-structured-metapopulations-read-it-here-10-48550-arxiv-2504-08433",
           title: 'A new pre-print collaborating with colleagues at Virginia Tech is out! We investigate...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-metapopulations",
+          title: 'Metapopulations',
+          description: "Research into the evolution of populations on metapopulations subject to time-varying environments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-cooperative-amr",
+          title: 'Cooperative AMR',
+          description: "Research into the evolution of cooperative antimicrobial resistance subject to time-varying environments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-twofold-ev",
+          title: 'Twofold EV',
+          description: "Research into the evolution of a population subject to an environment which varies in time in two distinct ways.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-stress-induced-mutagenesis",
+          title: 'Stress-induced mutagenesis',
+          description: "Research investigating increased mutation rates in populations during periods of stress",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-tennis",
+          title: 'Tennis',
+          description: "A project inspired by Roger Federer&#39;s 2024 Dartmouth Commencement speech",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-bluesky',
         title: 'Bluesky',
