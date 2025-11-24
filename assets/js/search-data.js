6627e6ed16c180ecd26af415c46d105bb767a5b6
mattasker <36448551+mattasker@users.noreply.github.com>
2025-11-24 10:41:42 +0000

    Deploying to gh-pages from @ mattasker/mattasker.github.io@6d3f99255607d6858f2599f4590f9015793d65c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,7 +68,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-gave-an-invited-talk-on-time-fluctuating-metapopulations-fixation-and-extinction-at-the-mini-symposium-spatial-and-temporal-models-of-ecology-and-evolution-of-the-bmc-bamc-2025-in-exeter-uk",
           title: 'I gave an invited talk on “Time-fluctuating metapopulations: fixation and extinction” at the...',
           description: "",
-          section: "News",},{id: "news-i-successfully-passed-my-phd-viva-with-editorial-corrections-to-my-thesis-many-thanks-to-my-external-examiners-george-constable-and-steve-fitzgerald-for-making-the-whole-thing-a-pleasant-experience",
+          section: "News",},{id: "news-i-successfully-passed-my-phd-viva-with-editorial-corrections-to-my-thesis-many-thanks-to-my-external-examiners-george-constable-and-steve-fitzgerald-for-making-the-whole-thing-a-pleasant-experience-my-thesis-can-be-found-here",
           title: 'I successfully passed my PhD viva with editorial corrections to my thesis! Many...',
           description: "",
           section: "News",},{id: "news-i-have-started-as-a-postdoctoral-researcher-in-the-theoretical-biology-group-at-the-max-planck-institute-for-evolutionary-biology-working-with-arne-traulsen-i-will-be-investigating-stress-induced-mutagenesis-on-a-hfsp-funded-project-working-in-collaboration-with-experimentalists-in-the-martin-kupiec-lab-at-tel-aviv-university",
